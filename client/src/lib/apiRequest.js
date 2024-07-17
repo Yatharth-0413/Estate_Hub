ca5eec0d5bb8fd7971068cc31f732001f5b9a091
YATHARTH BHARDWAJ <125243416+Yatharth-0413@users.noreply.github.com>
2024-07-17 21:55:38 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://estate-hub.onrender.com",
   withCredentials: true,
 });
 
